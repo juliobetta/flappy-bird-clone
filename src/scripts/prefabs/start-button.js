044ddb309af17ac3bc87287cd22300fd32ae8392
Julio Betta <juliobetta@gmail.com>
2015-06-17 15:03:29 -0300

    replace traceur by babel

--- src/scripts/prefabs/start-button.js
+++ src/scripts/prefabs/start-button.js
@@ -3,7 +3,9 @@ class StartButton extends Phaser.Button {
   constructor(game, x, y) {
     x = x || game.width / 2;
     y = y || 300;
-
+    try {
+      super();
+    } catch(e) {}
     super(game, x, y, 'startButton', this.onClick, this);
 
     this.anchor.setTo(0.5, 0.5);
--- src/scripts/states/preload.js
+++ src/scripts/states/preload.js
@@ -20,15 +20,15 @@ class Preload {
     // preloading assets ...
 
     // images
-    this.load.image('background',   'assets/background.png');
-    this.load.image('ground',       'assets/ground.png');
-    this.load.image('title',        'assets/title.png');
-    this.load.image('startButton',  'assets/start-button.png');
-    this.load.image('instructions', 'assets/instructions.png');
-    this.load.image('getReady',     'assets/get-ready.png');
-    this.load.image('gameover',     'assets/gameover.png');
-    this.load.image('scoreboard',   'assets/scoreboard.png');
-    this.load.image('particle',     'assets/particle.png');
+    this.load.image('background',   'assets/images/background.png');
+    this.load.image('ground',       'assets/images/ground.png');
+    this.load.image('title',        'assets/images/title.png');
+    this.load.image('startButton',  'assets/images/start-button.png');
+    this.load.image('instructions', 'assets/images/instructions.png');
+    this.load.image('getReady',     'assets/images/get-ready.png');
+    this.load.image('gameover',     'assets/images/gameover.png');
+    this.load.image('scoreboard',   'assets/images/scoreboard.png');
+    this.load.image('particle',     'assets/images/particle.png');
 
     // fonts
     this.load.bitmapFont(
@@ -36,15 +36,15 @@ class Preload {
     );
 
     // sounds
-    this.load.audio('score',     'assets/score.wav');
-    this.load.audio('flap',      'assets/flap.wav');
-    this.load.audio('pipeHit',   'assets/pipe-hit.wav');
-    this.load.audio('groundHit', 'assets/ground-hit.wav');
+    this.load.audio('score',     'assets/sounds/piu.wav');
+    this.load.audio('flap',      'assets/sounds/boing.wav');
+    this.load.audio('pipeHit',   'assets/sounds/pff.wav');
+    this.load.audio('groundHit', 'assets/sounds/kaploft.wav');
 
     // sprite sheets
-    this.load.spritesheet('bird',   'assets/bird.png', 34, 24, 3);
-    this.load.spritesheet('pipe',   'assets/pipes.png', 54, 320, 2);
-    this.load.spritesheet('medals', 'assets/medals.png', 44, 46, 2);
+    this.load.spritesheet('bird',   'assets/images/bird.png', 34, 24, 3);
+    this.load.spritesheet('pipe',   'assets/images/pipes.png', 54, 320, 2);
+    this.load.spritesheet('medals', 'assets/images/medals.png', 44, 46, 2);
   }
 
 
